8bc69cdda0328914f33e890c82df7d93b83707d3
Pramod Vadrevu <pramodvadrevu@gmail.com>
2017-04-15 11:34:44 -0500

    Adding mongoose example.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Spring2017Day4
 Day-4 of the class
+
+
+// blogs / posts / comments /
+// CRUD  | mongoDB | REST
+// ----- |---------|-------
+//create | insert  | POST
+// read  | find    | GET
+// update| update  | PUT
+// delete| remove  | DELETE
+
+// server.verb('path', function(req, res, next){})
+
+
+// curl -X GET localhost:8080/
+// curl -X DELETE localhost:8080/
+// curl -s -X GET localhost:8080/hello/pramod => meta info on the file.
--- index.js
+++ index.js
@@ -1,21 +1,77 @@
 var restify = require('restify');
 var server = restify.createServer();
 var port = 8080;
-// blogs / posts / comments /
-// CRUD  | mongoDB | REST
-// ----- |---------|-------
-//create | insert  | POST
-// read  | find    | GET
-// update| update  | PUT
-// delete| remove  | DELETE
+var mongoose = require ('mongoose');
+mongoose.connect('mongodb://localhost/enron');
+var db = mongoose.connection;
+var fs = require('fs');
 
-// server.verb('path', function(req, res, next){})
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+	_id: Schema.Types.ObjectId,
+	sender: String,
+	recipients: [],
+	cc:[],
+	text: String,
+	mid: String,
+	fpath: String,
+	bcc: [],
+	to: [],
+	replyto: Schema.Types.Mixed,
+	ctype: String,
+	fname: String,
+	date: Date,
+	subject: String
+})
+
+var Emails = mongoose.model('emails',schema);
+
+db.on('error', function(msg){
+	console.log('Mongoose connection error %s', msg);
+});
+
+db.once('open', function(){
+	console.log('Mongoose connection established.');
+})
+
 
 function send(req, res, next){
 	res.send("Testing "+ req.params.test);
 	return next();
 }
-server.get('/',send);
+
+function getEmails(req, res, next){
+	Emails.find().limit(10).exec(function(err, data)
+	{
+		if(err){res.send('Error');}
+		else{
+			res.json(data);
+		}
+	});
+	return next();
+}
+server.get('/',function(req,res,ext){
+	fs.readFile('index.html',function(err,data){
+		if(err){
+			console.log("Cannot read file index.html");
+			res.send(404);
+		}
+		else
+		{
+			var body = data.toString();
+			res.writeHead(200, {
+				'Content-Length': Buffer.byteLength(body),
+				'Content-Type': 'text/html'
+			});
+			res.write(body);
+			res.end();
+		}
+		return next();
+	})
+});
+
+server.get('/emails',getEmails);
 
 server.get('/hello/:test', send);
 
@@ -34,7 +90,4 @@ server.del('/hello/:test', function(req, res, next){
 server.listen(port,function(){
 	console.log('%s listening at %s', server.name,port);
 });
-// curl -X GET localhost:8080/
-// curl -X DELETE localhost:8080/
-// curl -s -X GET localhost:8080/hello/pramod => meta info on the file.
 
